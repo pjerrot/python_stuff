b3b67d29cf6bb86a00b3d2098d8588cc8d7359b9
pjerrot <JWR@int.sonofon.dk>
2016-07-19 21:12:29 +0200

    stopcksim

--- stocksim/stocksim_script.py
+++ stocksim/stocksim_script.py
@@ -18,23 +18,44 @@ today_day = time.strftime("%d")
 today_month = time.strftime("%m")
 today_year = time.strftime("%Y")
 
-# GOOGLE VERSION: henturl = "http://www.google.com/finance/historical?cid=304466804484872&startdate=Jul+6%2C+2015&enddate=Jul+18%2C+2016&num=30&output=csv"
+
+conn = pymssql.connect(server='IVABCRM01\SQLEXPRESS', database='stocksim')
+cursor = conn.cursor()
+
 i = 0
 symbols = open('symbols.txt' ,'r')
 for line in symbols:
     i += 1
     ticker = line[0:line.find("\t")]
-    print(ticker)
-    henturl = "http://real-chart.finance.yahoo.com/table.csv?s=" + ticker + "&d=" + today_day + "&e=" + today_month + "&f=" + today_year + "&g=d&a=2&b=13&c=1986&ignore=.csv"
-    download_stock_date(henturl)
+    if ticker != "Ticker":
+        print(ticker)
+        try:
+            henturl = "http://real-chart.finance.yahoo.com/table.csv?s=" + ticker + "&d=" + today_day + "&e=" + today_month + "&f=" + today_year + "&g=d&a=2&b=13&c=1986&ignore=.csv"
+            download_stock_date(henturl)
 
+            prices = open('stock.csv' ,'r')
+            j = 0
+            for line in prices:
+                j += 1
+                if j>1 and len(line)>5 is not None:
+                    sql = "insert into stock(ticker, Dato, [Open], High, Low, [Close], Volume,Adj_Close) values('" + ticker + "'," + line + ")"
+                    cursor.execute(sql.replace("-", ""))
+                    conn.commit()
+        except:
+            print("Failed")
 
+cursor.close()
+conn.close()
 
+#############################
+print(sql)
+print(henturl)
 
-conn = pymssql.connect(server='IVABCRM01\SQLEXPRESS', database='stocksim')
-cursor = conn.cursor()
+# GOOGLE VERSION: henturl = "http://www.google.com/finance/historical?cid=304466804484872&startdate=Jul+6%2C+2015&enddate=Jul+18%2C+2016&num=30&output=csv"
 
 cursor.execute('SELECT * FROM stock')
+
+cursor.execute(sql.replace("-", "").replace("\n",""))
 
 rows = cursor.fetchall()
 
@@ -48,17 +69,11 @@ for row in rows:
 
 conn.commit()
 
-prices = open('stock.csv' ,'r')
-i = 0
-for line in prices:
-    i += 1
-    if i>1 and len(line)>5 is not None:
-        sql = "insert into stock(ticker, Dato, [Open], High, Low, [Close], Volume,Adj_Close) values('" + ticker + "'," + line + ")"
-        print(sql)
 
-print(sql)
 
-cursor.execute(sql)
+print(sql.replace("-",""))
+
+cursor.execute(sql.replace("-",""))
 
 len(line)
 #    ticker = line[0:line.find("\t")]
@@ -68,6 +83,3 @@ cursor.execute("insert into stock(ticker, Dato, [Open], High, Low, [Close], Volu
 cursor.execute("insert into stock(ticker, Dato, [Open], High, Low, [Close], Volume,Adj_Close) values('MSFT','20160102',100,120,90,110, 2000,2100)")
 conn.commit()
 
-cursor.close()
-
-conn.close()
